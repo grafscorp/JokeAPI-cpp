dbd0982607f519882cc5439a57ff45a4497c61f9
Grafscorp <golyandringraf@gmail.com>
2024-12-22 05:40:40 +0300

    Create test main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿// main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#define CURL_STATICLIB
+
+#include <iostream>
+
+#include "include/joke_client_api.h"
+
+
+int main()
+{
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    
+    JokeClient jClient;
+    jClient.setJokeLang("en");
+    jClient.setJokeCategory(JokeCategory::Any);
+    jClient.setJokeType(JokeType::single);
+    std::cout << jClient.getJokeData().getJoke();
+
+
+    curl_global_cleanup();
+}
+
