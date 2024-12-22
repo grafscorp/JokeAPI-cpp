1ff47c99a8171f100fe0cd5937fb6138cc6dde25
Grafscorp <golyandringraf@gmail.com>
2024-12-22 05:39:49 +0300

    Create joke_category.h

---
+++ include/joke_category.h
@@ -0,0 +1,23 @@
+#pragma once
+#define MAX_SIZE_JOKE_CATEGORY 7
+
+
+//Any, Mics,Programming,Dark,Pun,Spooky,Christams
+enum JokeCategory
+{
+    Any = 0,
+    Mics,
+    Programming,
+    Dark,
+    Pun,
+    Spooky,
+    Christmas
+};
+const std::string JOKE_CATEGORY_VALUES[MAX_SIZE_JOKE_CATEGORY] = {
+"Any",
+"Mics",
+"Programming",
+"Dark",
+"Pun",
+"Spooky",
+"Christmas" };
