72e3794c0dc3877ca1ce9728aae7e4c9c40dec42
Grafscorp <golyandringraf@gmail.com>
2024-12-22 05:40:17 +0300

    Create JokeProperties

---
+++ include/JokeProperties.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "joke_data.h"
+class JokeProperties
+{
+public:
+	JokeProperties();
+	~JokeProperties();
+    //Any, Mics, Programming, Dark, Pun, Spooky, Chrostmas
+    void setJokeCategory(JokeCategory);
+    void setJokeType(JokeType);
+    void setJokeLang(const char[3]);
+    void setJokeBlacklistFlags(std::string);
+    void setJokeBlacklistFlags(std::vector<std::string>);
+    std::string getLangParam();
+    std::string getTypeParam();
+    std::string getParams();
+protected:
+	std::string j_lang = "en";
+    std::string j_category = "Any";
+    std::vector<std::string> j_blackflags;
+    JokeType j_type = JokeType::single;
+
+
+};
