1b6682062d8cfd48bcbd115e736359874b06d7ae
Grafscorp <golyandringraf@gmail.com>
2024-12-23 21:39:23 +0300

    Create exmaple.cpp

---
+++ exmaple/exmaple.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <JokeClientApi.h>
+
+
+int main(void)
+{
+
+	JokeClient jClient;
+	jClient.setJokeLang("en");
+	jClient.setJokeCategory(JokeCategory::Programming);
+	jClient.setJokeType(JokeType::single);
+
+	JokeData jData = jClient.getJokeData();
+	std::cout << jData.getJoke();
+	return 0;
+}
